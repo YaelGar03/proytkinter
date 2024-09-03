ae8dfac761a172f09395ff5aab0a1b52559a8ab4
Yael Garcia F. <72664493+YaelGar03@users.noreply.github.com>
2024-09-02 21:30:21 -0300

    Avances  proyecto

---
+++ interfaz.py
@@ -0,0 +1,62 @@
+class InterfazSesion:
+    def __init__(self):
+        self.usuario=input("Ingrese su usuario: ")
+        self.contrasena=input("Ingrese su contraseña: ")
+
+
+class Interfaz:
+    def __init__(self):
+      #Funciones
+      # def Texto(text)
+      # def Campos para escribir(args)
+      # def lista desplegable(args)
+      # def Botones(text, command)
+      # def cuadro para mostrar resumen registros, lista de miembros(?)
+      # def total_registros
+      # def Grafico resumen
+
+      # def barra de titulo custom, si es posible.
+        # Hay que desactivar la barra original,
+        # y armar una propia para cambiar colores
+
+
+class InterfazPrincipal:
+    def __init__(self):
+      #Columna 1
+      # llamar def texto de bienvenida
+      # llamar listas desplegables
+      # llamar campos para escribir
+      # llamar def Botones
+
+      #Columna 2
+      # llamar cuadro resumen registros
+      # llamar total_registros
+      # llamar grafico
+
+class  InterfazEditarGrupoFamiliar:
+    def __init__(self):
+      # llamar texto
+      # llamar cuadro miembros del grupo familiar registrados
+      # llamar campos para escribir
+      # llamar def Botones
+
+class  InterfazEditarRegistros:
+    def __init__(self):
+      # llamar texto
+      # llamar cuadro registros
+      # llamar def Botones
+
+    class BorrarRegistros:
+      def __init__(self):
+        # llamar texto
+        # llamar cuadro registros
+        # llamar def Botones
+
+    class EditarRegistros:
+      def __init__(self):
+        # llamar texto
+        # llamar cuadro registros
+        # llamar lista desplegable
+        # llamar campos para escribir
+        # llamar def Botones
+
