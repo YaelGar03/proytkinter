1bef55355b51beb39da1677c33dbc023e18097ec
Yael Garcia F. <72664493+YaelGar03@users.noreply.github.com>
2024-09-02 21:43:39 -0300

    Avance

--- main.py
+++ main.py
@@ -1,1 +1,47 @@
-# Holis
+# Main.py
+# integrará todos las funciones y clases que existen en los otros archivos que importaremos.
+
+import interfaz.py
+import login.py
+import manejo_registros.py
+
+# Nuestros Usuarios de prueba. Esto tendriamos que almacenar en otro lado, no en el código.
+credenciales='''Yael,1234
+Cecilia,5678'''
+
+
+### Pantalla de inicio ###
+# Obtenesmos usuario y contraseña a través de la interfaz Tkinter, interfaz.py
+# Esta sería lo haríamos por terminal según yo
+registrarse_o_iniciarsesion=input("1. Iniciar sesión\n2. Registrarse\n")
+
+#Para iniciar sesión
+if registrarse_o_iniciarsesion=="1":
+  interfaz_inicio_sesion = InterfazSesion() # Se crea una instancia de la clase interfaz
+  usuario = interfaz_inicio_sesion.usuario
+  contrasena = interfaz_inicio_sesion.contrasena
+  # Se crea una instancia de la clase InicioSesion con los datos ingresados por el usuario en interfaz.py
+  inicio_sesion = InicioSesion(usuario, contrasena)
+
+#Para regiustrarse
+if registrarse_o_iniciarsesion=="2":
+  interfaz_registro_usuario = InterfazSesion() # Se crea una instancia de la clase interfaz
+  usuario = interfaz_registro_usuario.usuario
+  contrasena = interfaz_registro_usuario.contrasena
+  # Se crea una instancia de la clase InicioSesion con los datos ingresados por el usuario en interfaz.py
+  registro_usuario = RegistroUsuario(usuario, contrasena)
+  print(credenciales)
+
+
+
+### Pantalla Principal ###
+
+# Interfaz()
+# InterfazPrincipal()
+# InterfazEditarGrupoFamiliar()
+# InterfazEditarRegistros()
+# InterfazEditarRegistros.BorrarRegistros()
+# InterfazEditarRegistros.EditarRegistros()
+
+# Tenemos que captar datos del usuario y de nuestro archivo .xlsx(excel) de nuuestra INTERFAZ. 
+# Luego los enviaremos nuestro archivo manejo_registros.py para guardarlos, editar la información que ya tenemos o borrarla.
