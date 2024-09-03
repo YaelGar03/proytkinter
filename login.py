7318a729cba26205b0f684ce72f9ca1d530a13b6
Yael Garcia F. <72664493+YaelGar03@users.noreply.github.com>
2024-09-02 21:29:03 -0300

    Avance proyecto

---
+++ login.py
@@ -0,0 +1,36 @@
+class InicioSesion:
+    def __init__(self, usuario, contrasena):
+        self.usuario1 = usuario
+        self.contrasena1 = contrasena
+        self.validar_credenciales()
+
+    def validar_credenciales(self):
+        for linea in credenciales.splitlines():
+          usuario, contrasena = linea.strip().split(',')
+          if usuario == self.usuario1 and contrasena == self.contrasena1:
+            print("Inicio de sesión exitoso")
+            return True
+          else:
+            print("Inicio de sesión fallido")
+            return False
+
+
+
+class RegistroUsuario:
+    def __init__(self,usuario,contrasena):
+        self.usuario=usuario
+        self.contrasena=contrasena
+        self.registrar_usuario()
+
+    def registrar_usuario(self):
+        global credenciales
+        credenciales= credenciales+"\n"+usuario+","+contrasena
+        print("Usuario registrado exitosamente")
+        return credenciales
+
+
+        # Almacenamos en credenciales.txt usuario y contraseña
+#        with open(credenciales.txt, 'a') as credenciales:
+#        credenciales.write(f'{usuario},{contrasena}\n')
+
+
